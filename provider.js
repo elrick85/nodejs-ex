e0e324c8599280847e8b4cfff06be69582a20c2b
Zaur Ismailov <zaur_ismailov@epam.com>
2016-11-10 13:35:45 +0300

    Fixed provider.

--- provider.js
+++ provider.js
@@ -4,69 +4,75 @@
 
 var loki = require("lokijs");
 var Q = require("q");
+var path = require("path");
 
 module.exports = {
-    getList: function(options) {
+  getDbPath: function() {
+    return path.join(__dirname, 'db.json');
+  },
+  getList: function(options) {
 
-        var _options = {
-            offset: options.offset ? Number(options.offset) : 0,
-            limit: options.limit ? Number(options.limit) : 1
-        };
+    var _options = {
+      offset: options.offset ? Number(options.offset) : 0,
+      limit: options.limit ? Number(options.limit) : 1
+    };
 
-        var defer = Q.defer();
-        var db = new loki('./db.json');
+    var defer = Q.defer();
+    var dbPath = this.getDbPath();
+    var db = new loki(dbPath);
 
-        db.loadDatabase({}, function(res) {
-            var col = db.getCollection("words");
-            var resultset = col.chain().find().branch();
-            var total = resultset.count();
+    db.loadDatabase({}, function(res) {
+      var col = db.getCollection("words");
+      var resultset = col.chain().find({}).branch();
+      var total = resultset.count();
 
-            var data = resultset
-                .offset(_options.offset)
-                .limit(_options.limit)
-                .data();
+      var data = resultset
+        .offset(_options.offset)
+        .limit(_options.limit)
+        .data();
 
-            defer.resolve({
-                data: data,
-                pagination: {
-                    offset: _options.offset,
-                    limit: _options.limit,
-                    total: total
-                }
-            });
+      defer.resolve({
+        data: data,
+        pagination: {
+          offset: _options.offset,
+          limit: _options.limit,
+          total: total
+        }
+      });
+    });
+
+    return defer.promise;
+  },
+
+  add: function(data) {
+    var defer = Q.defer();
+    var dbPath = this.getDbPath();
+    var db = new loki(dbPath);
+    var errors = [];
+
+    db.loadDatabase({}, function(res) {
+      var col = db.getCollection("words");
+      if(data.length) {
+        data.forEach(function(v) {
+          try {
+            col.insert(v);
+          }
+          catch(e) {
+            errors.push({ message: e.message });
+          }
         });
 
-        return defer.promise;
-    },
+        db.saveDatabase();
+      }
 
-    add: function(data) {
-        var defer = Q.defer();
-        var db = new loki('./db.json');
-        var errors = [];
+      var sCount = data.length - errors.length;
 
-        db.loadDatabase({}, function(res) {
-            var col = db.getCollection("words");
-            if(data.length) {
-                data.forEach(function(v) {
-                    try {
-                        col.insert(v);
-                    }
-                    catch(e) {
-                        errors.push({ message: e.message });
-                    }
-                });
+      defer.resolve({
+        success: sCount,
+        errors: errors
+      });
+    });
 
-                db.saveDatabase();
-            }
-
-            var sCount = data.length - errors.length;
-
-            defer.resolve({
-                success: sCount,
-                errors: errors
-            });
-        });
-
-        return defer.promise;
-    }
+    return defer.promise;
+  }
 };
